f2ca1c7c0d4baddeea078743d521d6a87de60328
nathanaelhoun <contact@nathanaelhoun.fr>
2019-06-21 12:52:10 +0200

    Ajout d'un champ caché dans le formulaire de mail pour éviter des spams

--- assets/php/form.php
+++ assets/php/form.php
@@ -4,40 +4,44 @@ require 'functions.php';
 
 //Récupération des variables
 $name = htmlentities($_POST['name']);
-$email = htmlentities($_POST['email']);
+$email = htmlentities($_POST['email2']);
 $subject = htmlentities($_POST['subject']);
 $message = htmlentities($_POST['message']);
+$shouldBeEmpty = htmlentities($_POST["email"]);
 
+if (!isset($shouldBeEmpty)) {
 
-//Forme de l'email à envoyer 
-$headers = 'MIME-Version: 1.0'."\n";
-$headers .= 'Content-type: text/html; charset=utf-8'."\n";
-$headers .= 'From : '.$email;
+	//Forme de l'email à envoyer 
+	$headers = 'MIME-Version: 1.0' . "\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\n";
+	$headers .= 'From : ' . $email;
 
-$content = '
+	$content = '
 	<html>
 		<head>
 			<title> Nouvel email depuis nathanaelhoun.fr </title>
 		</head>';
-$content .= '
+	$content .= '
 		<body>
-			<p><strong>Nom :</strong> '.$name;
-$content .= '</p>
-			<p><strong>Adresse mail :</strong> ' .$email;
-$content .= '</p>
-			<p><strong>Sujet :</strong> '.$subject;
-$content .= '</p>
-			<p><strong>Message :</strong> '.$message;
-$content .= '</p></body></html>';
+			<p><strong>Nom :</strong> ' . $name;
+	$content .= '</p>
+			<p><strong>Adresse mail :</strong> ' . $email;
+	$content .= '</p>
+			<p><strong>Sujet :</strong> ' . $subject;
+	$content .= '</p>
+			<p><strong>Message :</strong> ' . $message;
+	$content .= '</p></body></html>';
 
 
-//Envoi du mail
-if(mail('contact@nathanaelhoun.fr', 'Nouvel email depuis nathanaelhoun.fr : '. $email, $content, $headers)) {
-	setFlash("<i class=\"icon fa-envelope\"></i>&nbsp;<strong> C'est envoyé ! </strong>Merci pour votre message, j'y répondrai dans les meilleurs délais. <br><br> ", "success");
+	//Envoi du mail
+	if (mail('contact@nathanaelhoun.fr', 'Nouvel email depuis nathanaelhoun.fr : ' . $email, $content, $headers)) {
+		setFlash("<i class=\"icon fa-envelope\"></i>&nbsp;<strong> C'est envoyé ! </strong>Merci pour votre message, j'y répondrai dans les meilleurs délais. <br><br> ", "success");
+	} else {
+		setFlash("<i class=\"icon fa-exclamation-triangle\"></i>&nbsp;<strong>Attention ! </strong>Une erreur s'est produite lors de l'envoi du mail... <br><br> ", "alert");
+	}
 } else {
+	// Le formulaire vide a été rempli, probablement un robot
 	setFlash("<i class=\"icon fa-exclamation-triangle\"></i>&nbsp;<strong>Attention ! </strong>Une erreur s'est produite lors de l'envoi du mail... <br><br> ", "alert");
 }
-
 //Redirection vers la page
 header('Location:' . '../../' . '#footer');
-?>
