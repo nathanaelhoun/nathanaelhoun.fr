8711fe043790f906329b924f1204d8e27b64078f
Nathanaël Houn <contact@nathanaelhoun.fr>
2021-04-10 20:08:39 +0200

    Correct contact from script to prevent empty emails and restore flash warning

--- docs/assets/php/form.php
+++ docs/assets/php/form.php
@@ -1,6 +1,5 @@
 <?php
 //Récupération des infos du formulaire, créé par Nathanaël Houn
-require 'functions.php';
 
 //Récupération des variables
 $name = htmlentities($_POST['name']);
@@ -9,39 +8,35 @@ $subject = htmlentities($_POST['subject']);
 $message = htmlentities($_POST['message']);
 $shouldBeEmpty = htmlentities($_POST["email"]);
 
-if (strcmp($shouldBeEmpty,"") == 0) {
-
-	//Forme de l'email à envoyer
-	$headers = 'MIME-Version: 1.0' . "\n";
-	$headers .= 'Content-type: text/html; charset=utf-8' . "\n";
-	$headers .= 'From : ' . $email;
-
-	$content = '
-	<html>
-		<head>
-			<title> Nouvel email depuis nathanaelhoun.fr </title>
-		</head>';
-	$content .= '
-		<body>
-			<p><strong>Nom :</strong> ' . $name;
-	$content .= '</p>
-			<p><strong>Adresse mail :</strong> ' . $email;
-	$content .= '</p>
-			<p><strong>Sujet :</strong> ' . $subject;
-	$content .= '</p>
-			<p><strong>Message :</strong> ' . $message;
-	$content .= '</p></body></html>';
-
-
-	//Envoi du mail
-	if (mail('contact@nathanaelhoun.fr', 'Nouvel email depuis nathanaelhoun.fr : ' . $email, $content, $headers)) {
-		setFlash("<i class=\"icon fa-envelope\"></i>&nbsp;<strong> C'est envoyé ! </strong>Merci pour votre message, j'y répondrai dans les meilleurs délais. <br><br> ", "success");
-	} else {
-		setFlash("<i class=\"icon fa-exclamation-triangle\"></i>&nbsp;<strong>Attention ! </strong>Une erreur s'est produite lors de l'envoi du mail... <br><br> ", "alert");
-	}
-} else {
-	// Le formulaire vide a été rempli, probablement un robot
-	setFlash("<i class=\"icon fa-exclamation-triangle\"></i>&nbsp;<strong>Attention ! </strong>Une erreur s'est produite lors de l'envoi du mail... Vous semblez être un robot !  '".$shouldBeEmpty."' <br><br>", "alert");
+if (strlen($shouldBeEmpty) > 0 || strlen($name . $email . $subject . $message) == 0) {
+	// Le formulaire vide a été rempli, probablement un robot : redirection
+	header('Location:' . $_SERVER['HTTP_REFERER'] . '#footer-message-not-sent');
+	exit;
 }
-//Redirection vers la page
-header('Location:' . '../../' . '#footer');
+
+//Forme de l'email à envoyer
+$headers = 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\n";
+$headers .= 'From : ' . $email;
+
+$content = <<<CONTENT
+<html>
+	<head>
+		<title>Nouvel email depuis nathanaelhoun.fr</title>
+	</head>
+	<body>
+		<p><strong>Nom : </strong>$name</p>
+		<p><strong>Adresse mail : </strong>$email</p>
+		<p><strong>Sujet : </strong>$subject</p>
+		<p><strong>Message : </strong>$message</p>
+	</body>
+</html>
+CONTENT;
+
+//Envoi du mail
+if (mail('contact@nathanaelhoun.fr', 'Nouvel email depuis nathanaelhoun.fr : ' . $email, $content, $headers)) {
+	header('Location:' . $_SERVER['HTTP_REFERER'] . '#footer-message-sent');
+	exit();
+}
+
+header('Location:' . $_SERVER['HTTP_REFERER'] . "#footer-message-not-sent?name=$name&email2=$email&subject=$subject&message=$message");
